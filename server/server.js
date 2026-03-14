eb2b5fed2191e064d3e6175e85e4d38ce52b0ac5
takurmamatha169@gmail.com <takurmamatha169@gmail.com>
2026-03-14 14:51:46 +0530

    Added Sreccnshort to Redme

--- server/server.js
+++ server/server.js
@@ -1,48 +1,4 @@
-// require("dotenv").config({ path: __dirname + "/.env" });
-// const express = require("express");
-// const mongoose = require("mongoose");
 
-
-
-// const dotenv = require("dotenv");
-// const app = require("./app");   // ✅ keep this
-
-// const orderRoutes = require("./routes/orderRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const productRoutes = require("./routes/productRoutes");
-// const cartRoutes = require("./routes/cartRoutes");
-
-// dotenv.config();
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("MongoDB Connected Successfully ✅");
-//   } catch (error) {
-//     console.error("MongoDB Connection Failed ❌");
-//     console.error(error.message);
-//     process.exit(1);
-//   }
-// };
-
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/uploads", express.static("uploads"));
-
-// const startServer = async () => {
-//   await connectDB();
-
-//   const PORT = process.env.PORT || 5000;
-
-//   app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT} 🚀`);
-//   });
-// };
-// console.log("MONGO URI:", process.env.MONGO_URI);
-// mongoose.connect("mongodb://127.0.0.1:27017/ecommerce")
-// startServer();
 require("dotenv").config(); // ✅ Load env FIRST
 
 const express = require("express");
